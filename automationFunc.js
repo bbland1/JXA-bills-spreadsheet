e8c4debaa41e2793df4629c65901d300b4366036
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-09-23 03:15:29 -0400

    changing to returns with Error so that the apple shortcut app can send a message

--- automationFunc.js
+++ automationFunc.js
@@ -19,21 +19,19 @@ function createRowLookupMap(column) {
 // the name run and input is the default set when adding JS to the shortcut
 function run(input) {
 	const currentDate = new Date();
-	
+
 	const updatedBillsInfo = [];
 	let updateMessage;
-	
+
 	if (!input || input.length === 0) {
-		updateMessage = 'No bills provided to update.';
-		updatedBillsInfo.push(updateMessage);
-		return updatedBillsInfo;
+		return 'Error: No bills provided to update';
 	}
-	
+
+	// Launch numbers in background to keep the user experience clean
 	const numbersApp = Application('Numbers');
+	numbersApp.includeStandardAdditions = true;
 	const table = numbersApp.documents['Sample Bill Spreadsheet Shortcut'].sheets['Shared Bills'].tables[0];
 	const columnName = 'Bill';
-
-	// * get the column numerical value that we are searching with (Bill)
 	const targetColumn = table.columns.byName(columnName);
 
 	// * get which bills needs to be updated 
@@ -47,105 +45,102 @@ function run(input) {
 
 	// * loop over the specific values that are being updated
 	for (const targetValue of targetValues) {
-		// * find the row of that value to do the updating
 		let currentRow = rowLookupMap[targetValue];
-
+		// * find the row of that value to do the updating
 		if (!currentRow) {
-			updateMessage = `${targetValue} could not be found.`;
+			return `Error: ${targetValue} could not be found.`
+		}
+
+		// * get the current values of other data to work with
+		const paymentFrequency = table.cells[`B${currentRow}`].value();
+		const currentDueDate = table.cells[`C${currentRow}`].value();
+		const paymentAmount = table.cells[`D${currentRow}`].value();
+		const payoffBalance = table.cells[`E${currentRow}`].value();
+
+		// * because the shortcut sends an update message want to be able to know what a bill is missing
+		if (paymentFrequency === null || currentDueDate === null || paymentAmount === null) {
+			const missingData = [];
+
+			if (paymentFrequency === null) {
+				missingData.push('Bill Frequency');
+			}
+
+			if (currentDueDate === null) {
+				missingData.push('Due Date');
+			}
+
+			if (paymentAmount === null) {
+				missingData.push('Payment');
+			}
+
+			updateMessage = `${targetValue} could not be updated due to missing data: ${missingData.join(', ')}.`;
 			updatedBillsInfo.push(updateMessage);
-
 		} else {
-			// * get the current values of other data to work with
-			const paymentFrequency = table.cells[`B${currentRow}`].value();
-			const currentDueDate = table.cells[`C${currentRow}`].value();
-			const paymentAmount = table.cells[`D${currentRow}`].value();
-			const payoffBalance = table.cells[`E${currentRow}`].value();
 
-			// * because the shortcut sends an update message want to be able to know what a bill is missing
-			if (paymentFrequency === null || currentDueDate === null || paymentAmount === null) {
-				const missingData = [];
+			// * need the date to be the numerical short hand to do the math with
+			let month = currentDueDate.getMonth();
+			let day = currentDueDate.getDate();
+			let year = currentDueDate.getFullYear();
 
-				if (paymentFrequency === null) {
-					missingData.push('Bill Frequency');
-				}
+			// * makes sure the shortcut can be ran whenever but it will only update a bill if the date it is ran is after the listed due date
+			if (currentDate >= currentDueDate) {
+				let updatedDueDate;
 
-				if (currentDueDate === null) {
-					missingData.push('Due Date');
-				}
-
-				if (paymentAmount === null) {
-					missingData.push('Payment');
-				}
-
-				updateMessage = `${targetValue} could not be updated due to missing data: ${missingData.join(', ')}.`;
-				updatedBillsInfo.push(updateMessage);
-			} else {
-
-				// * need the date to be the numerical short hand to do the math with
-				let month = currentDueDate.getMonth();
-				let day = currentDueDate.getDate();
-				let year = currentDueDate.getFullYear();
-
-				// * makes sure the shortcut can be ran whenever but it will only update a bill if the date it is ran is after the listed due date
-				if (currentDate >= currentDueDate) {
-					let updatedDueDate;
-
-					switch (paymentFrequency) {
-						case 'Monthly':
-							month = (month + 1) % 12;
-							if (month > 11) {
-								year++;
-							}
-							updatedDueDate = new Date(year, month, day);
-							break;
-						case 'Quarterly':
-							month = (month + 3) % 12;
-							if (month > 11) {
-								year++;
-							}
-							updatedDueDate = new Date(year, month, day);
-							break;
-						case 'Semi-Annually':
-							month = (month + 6) % 12;
-							if (month > 11) {
-								year++;
-							}
-							updatedDueDate = new Date(year, month, day);
-							break;
-						case 'Annually':
+				switch (paymentFrequency) {
+					case 'Monthly':
+						month = (month + 1) % 12;
+						if (month > 11) {
 							year++;
-							updatedDueDate = new Date(year, month, day);
-					}
+						}
+						updatedDueDate = new Date(year, month, day);
+						break;
+					case 'Quarterly':
+						month = (month + 3) % 12;
+						if (month > 11) {
+							year++;
+						}
+						updatedDueDate = new Date(year, month, day);
+						break;
+					case 'Semi-Annually':
+						month = (month + 6) % 12;
+						if (month > 11) {
+							year++;
+						}
+						updatedDueDate = new Date(year, month, day);
+						break;
+					case 'Annually':
+						year++;
+						updatedDueDate = new Date(year, month, day);
+				}
 
-					table.cells[`C${currentRow}`].value = updatedDueDate;
+				table.cells[`C${currentRow}`].value = updatedDueDate;
 
-					// * not all the bills have a balance so checked for if the balance isn't null to do the logic that is specific to those bills
-					if (payoffBalance != null) {
-						const updatedPayoffBalance = payoffBalance - paymentAmount;
+				// * not all the bills have a balance so checked for if the balance isn't null to do the logic that is specific to those bills
+				if (payoffBalance != null) {
+					const updatedPayoffBalance = payoffBalance - paymentAmount;
 
-						// this needed to be a template literal for the spreadsheet to register that this was a number and keep it's format
-						table.cells[`E${currentRow}`].value = `${updatedPayoffBalance}`;
+					// this needed to be a template literal for the spreadsheet to register that this was a number and keep it's format
+					table.cells[`E${currentRow}`].value = `${updatedPayoffBalance}`;
 
-						updateMessage = `${targetValue} due date updated to ${updatedDueDate.toLocaleString('default', { month: 'numeric', day: 'numeric', year: 'numeric' })} and balance updated to ${updatedPayoffBalance}.`;
+					updateMessage = `${targetValue} due date updated to ${updatedDueDate.toLocaleString('default', { month: 'numeric', day: 'numeric', year: 'numeric' })} and balance updated to ${updatedPayoffBalance}.`;
 
-						updatedBillsInfo.push(updateMessage);
-					} else {
-
-						updateMessage = `${targetValue} due date updated to ${updatedDueDate.toLocaleString('default', { month: 'numeric', day: 'numeric', year: 'numeric' })}.`;
-
-						updatedBillsInfo.push(updateMessage);
-					}
+					updatedBillsInfo.push(updateMessage);
 				} else {
 
-					updateMessage = `${targetValue} due date (${currentDueDate.toLocaleString('default', { month: 'numeric', day: 'numeric', year: 'numeric' })}) has not passed yet.`;
+					updateMessage = `${targetValue} due date updated to ${updatedDueDate.toLocaleString('default', { month: 'numeric', day: 'numeric', year: 'numeric' })}.`;
 
 					updatedBillsInfo.push(updateMessage);
 				}
+			} else {
+
+				updateMessage = `${targetValue} due date (${currentDueDate.toLocaleString('default', { month: 'numeric', day: 'numeric', year: 'numeric' })}) has not passed yet.`;
+
+				updatedBillsInfo.push(updateMessage);
 			}
 		}
-	}
 
-	// * should save the spreadsheet when done but it should still be open to check
-	numbersApp.documents['Sample Bill Spreadsheet Shortcut'].save();
-	return updatedBillsInfo;
+		// * should save the spreadsheet when done but it should still be open to check
+		numbersApp.documents['Sample Bill Spreadsheet Shortcut'].save();
+		return updatedBillsInfo;
+	}
 }
